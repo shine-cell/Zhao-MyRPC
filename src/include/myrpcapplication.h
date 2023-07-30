33aa77c9cd3f85ae1a0aaad028dee940af8ad760
shine-cell <494640537@qq.com>
2023-07-30 23:42:33 +0800

    规范代码

--- src/include/myrpcapplication.h
+++ src/include/myrpcapplication.h
@@ -1,18 +1,19 @@
 #pragma once
 
-#include"myrpcconfig.h"
-#include"myrpccontroller.h"
-#include"myrpcchannel.h"
-//myrpc框架的基础类，负责框架的一些初始化操作，使用单例模式设计
+#include "myrpcconfig.h"
+#include "myrpccontroller.h"
+#include "myrpcchannel.h"
+// myrpc框架的基础类，负责框架的一些初始化操作，使用单例模式设计，包含整个框架的配置信息和日志信息
 class myrpcApplication
 {
 public:
-    static void Init(int argc,char **argv);
-    static myrpcApplication& GetInstance();
-    static myrpcConfig& GetConfig();
+    static void Init(int argc, char **argv);
+    static myrpcApplication &GetInstance();
+    static myrpcConfig &GetConfig();
+
 private:
     static myrpcConfig m_config;
     myrpcApplication(){};
-    myrpcApplication(const myrpcApplication&)=delete;
-    myrpcApplication(myrpcApplication&&)=delete;
+    myrpcApplication(const myrpcApplication &) = delete;
+    myrpcApplication(myrpcApplication &&) = delete;
 };
--- src/include/myrpcchannel.h
+++ src/include/myrpcchannel.h
@@ -1,13 +1,14 @@
 #pragma once
-#include<google/protobuf/service.h>
-#include<google/protobuf/descriptor.h>
-#include<google/protobuf/message.h>
+#include <google/protobuf/service.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/message.h>
 
-class myrpcChannel:public google::protobuf::RpcChannel
+class myrpcChannel : public google::protobuf::RpcChannel // RpcChannel是在google/protobuf/service.h头文件中定义的
 {
 public:
-    //所有通过stub代理对象调用的rpc方法都从这里处理，统一做方法调用的数据序列化和网络发送
-    void CallMethod(const google::protobuf::MethodDescriptor* method,
-                          google::protobuf::RpcController* controller, const google::protobuf::Message* request,
-                          google::protobuf::Message* response, google::protobuf::Closure* done);
+    // 所有通过stub代理对象调用的rpc方法都从这里处理，统一做方法调用的数据序列化和网络发送
+    // RpcChannel重写CallMethod方法
+    void CallMethod(const google::protobuf::MethodDescriptor *method,
+                    google::protobuf::RpcController *controller, const google::protobuf::Message *request,
+                    google::protobuf::Message *response, google::protobuf::Closure *done);
 };
