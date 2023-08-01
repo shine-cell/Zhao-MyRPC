4c81653758c8288b0d621320c9a1efd1ba2f5210
shine-cell <494640537@qq.com>
2023-08-01 21:26:49 +0800

    规范代码

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #！/bin/bash
 
-set -e
+set -e  # 表示在脚本执行过程中，一旦出现非零的返回值，立即停止脚本执行，避免出现错误继续执行的情况。
 
-rm -rf `pwd`/build/*
-cd `pwd`/build &&
+rm -rf `pwd`/build/* #删除项目根目录下的 build 文件夹及其下的所有内容。
+cd `pwd`/build && 
     cmake .. &&
     make
 cd ..
--- src/include/lockqueue.h
+++ src/include/lockqueue.h
@@ -1,37 +1,40 @@
 #pragma once
-#include<queue>
-#include<thread>
-#include<mutex>//对应linux下的pthread_mutex_t
-#include<condition_variable>//对应linux下的pthread_condition_t
+#include <queue>
+#include <thread>
+#include <mutex>              //对应linux下的pthread_mutex_t
+#include <condition_variable> //对应linux下的pthread_condition_t
 
-//异步写日志的日志队列
-template<typename T>
+// 异步写日志的日志队列
+template <typename T>
 class LockQueue
 {
 public:
-    //多个worker线程都会写日志queue
+    // 多个worker线程都会写日志queue
     void Push(const T &data)
     {
+        // 当 std::lock_guard 对象 lock 超出作用域时（即离开代码块），
+        // 它的析构函数会自动释放 m_mutex，解锁互斥锁，允许其他线程继续访问共享资源。
         std::lock_guard<std::mutex> lock(m_mutex);
         m_queue.push(data);
         m_condvariable.notify_one();
     }
 
-    //一个线程读日志queue
+    // 一个线程读日志queue
     T Pop()
     {
         std::unique_lock<std::mutex> lock(m_mutex);
-        while(m_queue.empty())
+        while (m_queue.empty())
         {
-            //日志队列为空，线程进入wait状态
+            // 日志队列为空，线程进入wait状态
             m_condvariable.wait(lock);
         }
-        
-        //不为空时开始处理
-        T data=m_queue.front();
+
+        // 不为空时开始处理
+        T data = m_queue.front();
         m_queue.pop();
         return data;
     }
+
 private:
     std::queue<T> m_queue;
     std::mutex m_mutex;
